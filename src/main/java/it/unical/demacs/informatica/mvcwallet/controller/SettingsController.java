d9e41e55bde4deff156a869874bf29a96ef8d27b
Ranchoo28 <savcreaa.kr99@gmail.com>
2023-07-05 09:48:36 +0200

    Fix e aggiunte per quanto riguarda l'ora.

--- src/main/java/it/unical/demacs/informatica/mvcwallet/controller/SettingsController.java
+++ src/main/java/it/unical/demacs/informatica/mvcwallet/controller/SettingsController.java
@@ -1,10 +1,12 @@
 package it.unical.demacs.informatica.mvcwallet.controller;
 
 import it.unical.demacs.informatica.mvcwallet.handler.SceneHandler;
+import it.unical.demacs.informatica.mvcwallet.handler.SettingsHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.RadioMenuItem;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 public class SettingsController {
@@ -15,36 +17,58 @@ public class SettingsController {
     private Label accessibilityLabel, accountLabel;
 
     @FXML
+    private RadioMenuItem italianLanguage, englishLanguage,
+            whiteTheme, darkTheme, mvcTheme,
+            h24Time, h12Time,
+            spotPage, marketPage,
+            eurCurrency, usdCurrency;
+
+    @FXML
+    void on12hClick() {
+        h12Choosen();
+        SettingsHandler.getInstance().format = "HH:mm:ss a";
+    }
+
+    @FXML
+    void on24hClick() {
+        h24Choosen();
+        SettingsHandler.getInstance().format = "HH:mm:ss";
+    }
+
+    @FXML
     void onCancelClick(){
         SceneHandler.getInstance().createSideBar();
     }
 
     @FXML
     void initialize(){
-
-        // Icona per l'account
-        FontIcon iconAccount = new FontIcon("mdi2a-account-cog");
-        iconAccount.setIconSize(30);
-        accountLabel.setContentDisplay(ContentDisplay.RIGHT);
-        accountLabel.setGraphic(iconAccount);
-
-        // Icona per l'accessibilità
-        FontIcon iconAccess = new FontIcon("mdi2h-human");
-        iconAccess.setIconSize(30);
-        accessibilityLabel.setContentDisplay(ContentDisplay.RIGHT);
-        accessibilityLabel.setGraphic(iconAccess);
+        if(SettingsHandler.getInstance().format.equals("HH:mm:ss")) h24Choosen();
+        if(SettingsHandler.getInstance().format.equals("HH:mm:ss a")) h12Choosen();
 
         // Icona per i temi
         FontIcon iconThemes = new FontIcon("mdi2t-theme-light-dark");
-        iconThemes.setIconSize(30);
+        iconThemes.setIconSize(25);
         themesMenu.setContentDisplay(ContentDisplay.RIGHT);
         themesMenu.setGraphic(iconThemes);
 
         // Icona per la lingua
         FontIcon iconLanguage = new FontIcon("mdi2e-earth");
-        iconLanguage.setIconSize(30);
+        iconLanguage.setIconSize(25);
         languageMenu.setContentDisplay(ContentDisplay.RIGHT);
         languageMenu.setGraphic(iconLanguage);
     }
 
+    private void h12Choosen(){
+        h12Time.setSelected(true);
+        h24Time.setSelected(false);
+
+    }
+
+    private void h24Choosen(){
+        h12Time.setSelected(false);
+        h24Time.setSelected(true);
+    }
+
+
 }
+
