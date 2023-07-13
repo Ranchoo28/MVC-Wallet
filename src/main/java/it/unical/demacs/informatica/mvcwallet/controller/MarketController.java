77416996a580c533f834cc1f5d3aee90d7829d42
Rhaegal222 <rhaegal222@gmail.com>
2023-07-13 13:16:51 +0200

    Tema blu quasi terminato e scene tutte sistemate

--- src/main/java/it/unical/demacs/informatica/mvcwallet/controller/MarketController.java
+++ src/main/java/it/unical/demacs/informatica/mvcwallet/controller/MarketController.java
@@ -9,7 +9,6 @@ import it.unical.demacs.informatica.mvcwallet.model.Coin;
 import it.unical.demacs.informatica.mvcwallet.view.CandleStickChart;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
