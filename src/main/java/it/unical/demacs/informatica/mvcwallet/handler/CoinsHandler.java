cf52145d156c81e67da3239a7e3fbe1f7074cb6b
Peter <Petertheking20122@gmail.com>
2023-07-13 23:25:10 +0200

    spots page

--- src/main/java/it/unical/demacs/informatica/mvcwallet/handler/CoinsHandler.java
+++ src/main/java/it/unical/demacs/informatica/mvcwallet/handler/CoinsHandler.java
@@ -12,10 +12,10 @@ public class CoinsHandler {
         return instance;
     }
 
-    private static final Coin btc = new Coin("Bitcoin", 0, "BTC");
-    private static final Coin eth = new Coin("Ethereum", 0, "ETH");
-    private static final Coin sol = new Coin("Solana", 0, "SOL");
-    private static final Coin bnb = new Coin("Binance Coin", 0, "BNB");
+    private static final Coin btc = new Coin("Bitcoin", 0, "BTC",0.0);
+    private static final Coin eth = new Coin("Ethereum", 0, "ETH",0.0);
+    private static final Coin sol = new Coin("Solana", 0, "SOL",0.0);
+    private static final Coin bnb = new Coin("Binance Coin", 0, "BNB",0.0);
 
     private final static Map<String, Coin> coins;
 
